99c483f08fcdc2995c6dbd676736081243d5df6a
Maxim Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-04-26 22:26:38 -0700

    Update hw3_3.cpp

--- hw3/hw3_3.cpp
+++ hw3/hw3_3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main()
+{
+
+    char response = 'y';
+
+    int max, min, number;
+
+    while (response == 'y')
+    {
+        max = -pow(2, 31);
+        min = pow(2, 31) - 1;
+        cout << "Enter a list of integers: " << endl;
+
+        while (cin.peek() != '\n')
+        {
+            cin >> number;
+            if (number < min)
+            {
+                min = number;
+            }
+
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        cout << "The max of the integers you entered is " << max << endl;
+        cout << "The min of the integers you entered is " << min << endl;
+
+        cout << "Continue? (y/n)" << endl;
+        cin >> response;
+        cin.ignore();
+    }
+    return 0;
+}
